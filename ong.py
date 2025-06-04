cf06a4e41ecc9f870ded6bef89c8b8b6bb255717
fabianosses <fabian.osses@gmail.com>
2025-06-04 02:02:51 +0000

    termina desarrollo de ong.py

--- ong.py
+++ ong.py
@@ -1,43 +1,32 @@
-import sys
-fact_ = []
-prod_ = []
+def calcular(**kwargs):
+  s = 0
+  for key, value in kwargs.items():
 
-def factorial ():
-    # Factorial:
-    # n! = n * n -1 * n -2 * ... *2 *1
-    # Ejemplo factorial:  5! = 5 * 4 * 3 * 2 * 1 = 120
+    # Condicional "if in"
+    if "fact_" in key:
 
-    n = int(input("ingrese un número entero positivo: "))
-    factorial = 1
-    i=0
-    while i < n:
-        print(i)
-        print (f"mostrará {n} veces")
+      # Factorial:
+      # n! = n * n -1 * n -2 * ... *2 *1
+      # Ejemplo factorial:  5! = 5 * 4 * 3 * 2 * 1 = 120
+
+      #n = int(input("ingrese un número entero positivo: "))
+      factorial = 1
+      i=0
+      while i < value:
         factorial = factorial * (i + 1)
-        print("fact: ", factorial)
         i+=1
-        print("fin")
+      print(f"El factorial de {value} es", factorial)
 
-def productoria ():
-    # Productoria:
-    #  Ejemplo: 𝐴 = [3,6,4,2,8]
-    #  ∏𝐴𝑖 =  3 * 6 * 4 * 2 * 8
+    # condicional "elif in"
+    elif "prod_" in key:
+      
+      # Productoria:
+      #  𝐴 = [3,6,4,2,8]
+      #  ∏𝐴𝑖 =  3 * 6 * 4 * 2 * 8
 
-    a = [4,6,7,4,3]
-    productoria = 1
-    for elemento in a:
+      productoria = 1
+      for elemento in value:
         productoria = productoria * elemento
-        print(productoria)
+      print(f"La productoria de {value} es", productoria)
 
-if len(sys.argv) != 1:
-    print("el comando necesita un argumento :  python ong.py")
-    sys.exit(1)
-
-n = len(sys.argv)
-for i in range (n):
-    if fact_(i) in sys.argv[i]:
-        factorial()
-    elif prod_(i) in sys.argv[i]:
-        productoria()
-
-calcular(fact_1 = 5, prod_1 = [3,6,4,2,8], fact_2 = 6)
+calcular(fact_1 = 5, prod_1 = [4,6,7,4,3], fact_2 = 6)
